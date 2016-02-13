40cf2d5131c826665af601b6b89e0d3463898996
Yves Talboom <yves.talboom@gmail.com>
2016-02-13 15:37:31 +0100

    Add format distance filter

---
+++ app_client/common/filters/formatDistance.filter.js
@@ -0,0 +1,27 @@
+var formatDistance = function () {
+
+    return function (distance) {
+        var numDistance, unit;
+        if (distance && _isNumeric(distance)) {
+
+            if (distance > 1000) {
+                numDistance = parseFloat(distance / 1000).toFixed(1);
+                unit = ' km';
+            } else {
+                numDistance = parseInt(distance, 10);
+                unit = ' m';
+            }
+            return numDistance + unit;
+        } else {
+            return "?";
+        }
+    };
+};
+
+var _isNumeric = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+angular
+    .module('loc8rApp')
+    .filter('formatDistance', formatDistance);
