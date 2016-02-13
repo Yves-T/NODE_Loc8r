1036deb5c5bfa676060a2502bc13d7840b473ea7
Yves Talboom <yves.talboom@gmail.com>
2016-02-13 15:51:54 +0100

    wrap rating stars directive in IIFE

--- app_client/common/directives/ratingstars/ratingStars.directive.js
+++ app_client/common/directives/ratingstars/ratingStars.directive.js
@@ -1,13 +1,17 @@
-angular
-    .module('loc8rApp')
-    .directive('ratingStars', ratingStars);
+(function () {
 
-function ratingStars() {
-    return {
-        restrict: 'EA',
-        scope: {
-            thisRating: '=rating'
-        },
-        templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
-    };
-}
+    angular
+        .module('loc8rApp')
+        .directive('ratingStars', ratingStars);
+
+    function ratingStars() {
+        return {
+            restrict: 'EA',
+            scope: {
+                thisRating: '=rating'
+            },
+            templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
+        };
+    }
+
+})();
