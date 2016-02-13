0e4b2664f03550707d75513640520877b899b30b
Yves Talboom <yves.talboom@gmail.com>
2016-02-13 15:53:04 +0100

    wrap geolocation service in IIFE

--- app_client/common/services/geolocation.service.js
+++ app_client/common/services/geolocation.service.js
@@ -1,16 +1,20 @@
-var geolocation = function () {
-    var getPosition = function (cbSuccess, cbError, cbNoGeo) {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
-        } else {
-            cbNoGeo();
-        }
-    };
-    return {
-        getPosition: getPosition
-    };
-};
+(function () {
 
-angular
-    .module('loc8rApp')
-    .service('geolocation', geolocation);
+    var geolocation = function () {
+        var getPosition = function (cbSuccess, cbError, cbNoGeo) {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
+            } else {
+                cbNoGeo();
+            }
+        };
+        return {
+            getPosition: getPosition
+        };
+    };
+
+    angular
+        .module('loc8rApp')
+        .service('geolocation', geolocation);
+
+})();
