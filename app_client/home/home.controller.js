ce0051859aeca9c2e97d76e7e28aea5e8566e9e0
Yves Talboom <yves.talboom@gmail.com>
2016-02-13 15:07:28 +0100

    Use controller as syntax for angular home controller

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -1,9 +1,10 @@
 function homeCtrl($scope) {
-    $scope.pageHeader = {
+    var vm = this;
+    vm.pageHeader = {
         title: 'Loc8r',
         strapline: 'Find places to work with wifi near you!'
     };
-    $scope.sidebar = {
+    vm.sidebar = {
         content: "Looking for wifi and a seat etc etc"
     };
 }
