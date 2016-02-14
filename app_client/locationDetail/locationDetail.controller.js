492382e9a6d3156cfe102ac15e0d571e5d642961
Yves Talboom <yves.talboom@gmail.com>
2016-02-14 13:15:55 +0100

    Send modal review data to api

--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -2,7 +2,7 @@
     angular
         .module('loc8rApp')
         .controller('locationDetailCtrl', locationDetailCtrl);
-    locationDetailCtrl.$inject = ['$routeParams', '$uibModal','loc8rData'];
+    locationDetailCtrl.$inject = ['$routeParams', '$uibModal', 'loc8rData'];
     function locationDetailCtrl($routeParams, $uibModal, loc8rData) {
         var vm = this;
         vm.locationid = $routeParams.locationid;
@@ -22,7 +22,21 @@
             var modalInstance = $uibModal.open({
                 templateUrl: '/reviewModal/reviewModal.view.html',
                 controller: 'reviewModalCtrl as vm',
+                resolve: {
+                    locationData: function () {
+                        return {
+                            locationid: vm.locationid,
+                            locationName: vm.data.location.name
+                        };
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (data) {
+                vm.data.location.reviews.push(data);
             });
         };
+
     }
+
 })();
