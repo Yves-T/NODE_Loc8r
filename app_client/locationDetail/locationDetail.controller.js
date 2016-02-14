3e214cbc18fe2676d9b14d1d24028f39f623414a
Yves Talboom <yves.talboom@gmail.com>
2016-02-14 10:45:23 +0100

    Add location detail controller

---
+++ app_client/locationDetail/locationDetail.controller.js
@@ -0,0 +1,11 @@
+(function () {
+    angular
+        .module('loc8rApp')
+        .controller('locationDetailCtrl', locationDetailCtrl);
+    function locationDetailCtrl() {
+        var vm = this;
+        vm.pageHeader = {
+            title: 'Location detail page'
+        };
+    }
+})();
