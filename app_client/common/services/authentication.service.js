c8a17087773c1efa4b8146e38c7899c3e4528752
Yves Talboom <yves.talboom@gmail.com>
2016-02-15 16:32:43 +0100

    Add user authentication functionality to authentication service

--- app_client/common/services/authentication.service.js
+++ app_client/common/services/authentication.service.js
@@ -1,7 +1,7 @@
 (function () {
 
-    authentication.$inject = ['$window'];
-    function authentication($window) {
+    authentication.$inject = ['$http', '$window'];
+    function authentication($http, $window) {
         var saveToken = function (token) {
             $window.localStorage['loc8r-token'] = token;
         };
@@ -10,9 +10,53 @@
             return $window.localStorage['loc8r-token'];
         };
 
+        var register = function (user) {
+            return $http.post('/api/register', user).success(function (data) {
+                saveToken(data.token);
+            });
+        };
+
+        var login = function (user) {
+            return $http.post('/api/login', user).success(function (data) {
+                saveToken(data.token);
+            });
+        };
+
+        var logout = function() {
+            $window.localStorage.removeItem('loc8r-token');
+        };
+
+        var isLoggedIn = function () {
+            var token = getToken();
+
+            if (token) {
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.exp > Date.now() / 1000;
+            } else {
+                return false;
+            }
+        };
+
+        var currentUser = function () {
+            if (isLoggedIn()) {
+                var token = getToken();
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+                return {
+                    email: payload.email,
+                    name: payload.name
+                };
+            }
+        };
+
         return {
+            currentUser: currentUser,
             saveToken: saveToken,
-            getToken: getToken
+            getToken: getToken,
+            isLoggedIn: isLoggedIn,
+            register: register,
+            login: login,
+            logout: logout
         };
     }
 
